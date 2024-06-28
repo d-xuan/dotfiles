c4bb64835d8139bbc652c95df94dc42981cd26f7
Darren Xuan <darren@workforceanalytics.com.au>
2024-06-29 03:06:58 +1000

    enable wireless

--- hardware/hardware-configuration.nix
+++ hardware/hardware-configuration.nix
@@ -11,6 +11,7 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+  hardware.enableRedistributableFirmware = lib.mkDefault true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
